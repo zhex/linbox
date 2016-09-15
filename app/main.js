0cf29325dfcaebeb2e35b2e85511f042d48c02e2
ZheX <zhex81@gmail.com>
2016-09-16 07:27:59 +0800

    disable page zoom

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
+import { webFrame } from 'electron';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app';
 
+webFrame.setZoomLevelLimits(1, 1);
 render(<App/>, document.getElementById('app'));
